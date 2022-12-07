a0253b075f6027bb496ef7cc931fcaa760e2e541
Pazzo97 <pazzomuk@gmail.com>
2022-12-07 16:04:26 +0200

    Edit header section

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -1,19 +1,19 @@
 import React from 'react'
 import { BsLinkedin } from 'react-icons/bs';
 import { FaGithub } from 'react-icons/fa';
-import { FaAngellist } from 'react-icons/fa';
+import { FaMediumM } from 'react-icons/fa';
 
 const HeaderSocials = () => {
   return (
 		<div className='header__socials'>
 			<a href='https://www.linkedin.com/in/patrick-mukunzi/' target='__blank'>
-				<BsLinkedin />
+				<BsLinkedin className='header__socials-icon' />
 			</a>
 			<a href='github.com/Pazzo97' target='__blank'>
-				<FaGithub />
+				<FaGithub className='header__socials-icon' />
 			</a>
-			<a href='https://angellist.com' target='__blank'>
-				<FaAngellist />
+			<a href='https://medium.com/@pazzomuk' target='__blank'>
+				<FaMediumM className='header__socials-icon' />
 			</a>
 		</div>
 	);
